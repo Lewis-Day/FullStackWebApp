6b1e13e4d1d866042e6f08145b2898c82736187d
Lewis Day <lewisday2901@outlook.com>
2025-03-03 16:07:15 +0000

    Social page progress
    Chats from users can now be fetched and deleted

--- backend/social/urls.py
+++ backend/social/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import addMessage, getMessages, createConversation
-
+from .views import addMessage, getMessages, createConversation, getConversations, deleteConversation
 urlpatterns = [
     path('api/createChat/', createConversation.as_view(), name='newChat'),
     path('api/sendMessage/', addMessage.as_view(), name='addMessage'),
     path('api/getMessages/', getMessages.as_view(), name='getMessages'),
+    path('api/getConversations/', getConversations.as_view(), name='getConversations'),
+    path('api/deleteChat/', deleteConversation.as_view(), name='deleteChat'),
 
 ]
