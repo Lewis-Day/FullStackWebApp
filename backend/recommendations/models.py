1462fc56fc9b384ea7ca507deefe4e8343ef164d
Lewis Day <lewisday2901@outlook.com>
2025-03-22 20:01:58 +0000

    Initial ratings page functionality/backend

--- backend/recommendations/models.py
+++ backend/recommendations/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class NewRatings(models.Model):
+
+    userID = models.IntegerField()
+    gameID = models.IntegerField()
+    rating = models.IntegerField()
--- backend/recommendations/urls.py
+++ backend/recommendations/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from .views import recommendationsView
+from .views import recommendationsView, initialRatingsView, addRatingsView
 
 urlpatterns = [
     path('api/recommendations/', recommendationsView.as_view(), name='recommendations'),
+    path('api/initialRatings/', initialRatingsView.as_view(), name='initialRatings'),
+    path('api/addRatings/', addRatingsView.as_view(), name='addRatings'),
 ]
