d2b0832c63af0b9560d33414fdfe9e6e411a649d
Lewis Day <lewisday2901@outlook.com>
2025-04-19 02:41:24 +0100

    edited styling of home page

--- frontend/src/app/Home/page.tsx
+++ frontend/src/app/Home/page.tsx
@@ -32,16 +32,16 @@ const Home = () => {
 
 
     return(
-        <div className="bg-gray-700 h-screen w-full pt-5 ">
-            <div className="navbar bg-white rounded-md mx-auto max-w-screen-xl">
+        <div className="bg-black h-screen w-full pt-5 ">
+            <div className="navbar bg-gray-800 bg-opacity-75 backdrop-blur-md rounded-md mx-auto max-w-screen-xl">
                 <div className="flex-1">
-                    <Link href="/Home" className="btn btn-ghost text-xl text-black">VGR</Link>
+                    <Link href="/Home" className="btn btn-ghost text-xl text-gray-100 hover:bg-gray-500 hover:text-cyan-400">VGR</Link>
                 </div>
                 <div className="flex-none">
-                    <ul className="menu menu-horizontal px-1 text-black">
-                    <li><Link href="/SignUp">Sign Up</Link></li>
+                    <ul className="menu menu-horizontal px-1 text-gray-100">
+                    <li><Link href="/SignUp" className="hover:bg-gray-500 hover:text-cyan-400">Sign Up</Link></li>
                     <li>
-                        <Link href="/Login">Login</Link>
+                        <Link href="/Login" className="hover:bg-gray-500 hover:text-cyan-400">Login</Link>
                     </li>
                     </ul>
                 </div>
@@ -50,27 +50,36 @@ const Home = () => {
             <div className="flex flex-col items-start content-center flex-wrap">
                 <h2 className="text-3xl text-gray-100 font-bold py-5">Popular Games</h2>
 
-                <div className="carousel carousel-center bg-neutral rounded-box max-w-screen-xl p-4">
+                <div className="carousel carousel-center bg-gray-800 bg-opacity-75 backdrop-blur-md rounded-box max-w-screen-xl p-4">
 
                     {imgs.length > 0 ? imgs.map((url, index) => (
                         <div key={index} className="carousel-item px-2">
-                            <img src={url} className="rounded-box hover:scale-[1.025] transition-transform" />
+                            <img src={url} className="object-cover rounded-box shadow-md hover:scale-[1.05] transition-transform " />
                         </div>
                     )) :
                     (
                         Array.from({length : numImages}).map((_, index) =>
                             <div key={index} className="carousel-item px-2">
-                                <div className="skeleton h-[22rem] w-[16.5rem]"></div>
+                                <div className="skeleton bg-gray-600 h-[22rem] w-[16.5rem]"></div>
                             </div>
                         )
                     )}
                 </div>
 
+                <div className="pt-2">
+                    <p className="text-gray-400 text-sm mb-1">
+                        Press <kbd className="kbd kbd-sm bg-gray-800 text-blue-400 border-none">▶︎</kbd> to scroll right.
+                    </p>
+                    <p className="text-gray-400 text-sm">
+                        Press <kbd className="kbd kbd-sm bg-gray-800 text-blue-400 border-none">◀︎</kbd> to scroll left.
+                    </p>
+                </div>
 
-                <div className="mockup-window border-base-300 border max-w-screen-xl p-4 mt-5 bg-gray-200 text-black w-full">
-                    <p className="text-xl font-bold pl-5">Welcome to VGR!</p>
+
+                <div className="mockup-window max-w-screen-xl p-4 mt-5 bg-gray-900 text-gray-100 w-full">
+                    <p className="text-xl font-bold pl-5 text-cyan-400">Welcome to VGR!</p>
                     <p className="pl-5">VGR is a video game recommendation system available to users of all platforms.</p>
-                    <p className="pl-5">Here you'll find popular games however, if you log in or create an account, you'll get personalised game recommedations.</p>
+                    <p className="pl-5">Here you'll find popular games however, if you <Link href="/Login" className="text-cyan-400 hover:underline">logging in</Link> or <Link href="/SignUp" className="text-cyan-400 hover:underline">creating an account</Link>, you'll get personalised game recommedations.</p>
                 </div>
             </div>
 
