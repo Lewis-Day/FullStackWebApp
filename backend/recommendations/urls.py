cd6d6fb41471678c5c6576b581ef250019e3b2d5
Lewis Day <lewisday2901@outlook.com>
2025-04-24 01:23:24 +0100

    Added wildcard game

--- backend/recommendations/urls.py
+++ backend/recommendations/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import recommendationsView, initialRatingsView, addRatingsView
+from .views import recommendationsView, initialRatingsView, addRatingsView, wildCardView
 
 urlpatterns = [
     path('api/recommendations/', recommendationsView.as_view(), name='recommendations'),
     path('api/initialRatings/', initialRatingsView.as_view(), name='initialRatings'),
     path('api/addRatings/', addRatingsView.as_view(), name='addRatings'),
+    path('api/wildcardRecommendation', wildCardView.as_view(), name='wildcard'),
 ]
